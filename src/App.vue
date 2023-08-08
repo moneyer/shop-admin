245e1214e20a87fd5fd5d0287edd15721097b1fb
moneyer <zhichao_java@163.com>
2023-08-09 07:00:42 +0800

    复制v3-admin-vite架构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+// import { h } from 'vue'
+import { useTheme } from '@/hooks/useTheme'
+// import { ElNotification } from 'element-plus'
+// 将 Element Plus 的语言设置为中文
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+const { initTheme } = useTheme()
+
+/** 初始化主题 */
+initTheme()
+
+/** 作者小心思 */
+// ElNotification({
+//   title: 'Hello',
+//   message: h(
+//     'a',
+//     {
+//       style: 'color: teal',
+//       target: '_blank',
+//       href: 'https://github.com/un-pany/v3-admin-vite',
+//     },
+//     '小项目获取 star 不易，如果你喜欢这个项目的话，欢迎点击这里支持一个 star ！这是作者持续维护的唯一动力（小声：毕竟是免费的）'
+//   ),
+//   duration: 0,
+//   position: 'bottom-right',
+// })
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <div>首页</div> -->
+  <ElConfigProvider :locale="zhCn">
+    <router-view />
+  </ElConfigProvider>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
